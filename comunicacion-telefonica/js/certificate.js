f5b2cbb2ad28e14a41a87f0f6bde300e0ce40dbe
tamaramilgar-tech <tamara.milgar@gmail.com>
2026-01-22 16:01:39 +0000

    Delete comunicacion-telefonica directory

--- comunicacion-telefonica/js/certificate.js
+++
@@ -1,7 +0,0 @@
-function generateCertificatePDF(){
-const {jsPDF}=window.jspdf;
-const doc=new jsPDF();
-doc.text("Certificado de aprovechamiento",20,20);
-doc.save("certificado.pdf");
-}
-document.getElementById("downloadCertBtn").onclick=generateCertificatePDF;
